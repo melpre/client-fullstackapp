1ae4620c4a7f2d02ae26aefa8d22d7082d199085
Melissa Preece <melissanchez@gmail.com>
2022-04-04 20:59:30 -0700

    3rd fix URL param

--- src/components/CourseDetail.js
+++ src/components/CourseDetail.js
@@ -33,7 +33,7 @@ export default function CourseDetail(props) {
             const urlParam = currentURL.substring(49);
             try {
                 // const response = await fetch(`http://localhost:5000/api/courses/${urlParam}`);
-                const response = await fetch(`https://server-courses-app.herokuapp.com/api/courses/${id}`); // Fetch from API hosted on Heroku
+                const response = await fetch(`https://server-courses-app.herokuapp.com/api/courses/${urlParam}`); // Fetch from API hosted on Heroku
                 // check what substring is being extracted
                 console.log(urlParam);
                 const data = await response.json();
